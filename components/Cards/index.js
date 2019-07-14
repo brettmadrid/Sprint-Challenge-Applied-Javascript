3bdce12e1bbf81954057cef4425a375029ee8b8b
brettmadrid <brettmadrid9@gmail.com>
2019-07-14 16:12:06 -0700

    added card component

---
+++ components/Cards/index.js
@@ -0,0 +1,57 @@
+// STEP 3: Create Article cards.
+// -----------------------
+// Send an HTTP GET request to the following address: https://lambda-times-backend.herokuapp.com/articles
+// Stduy the response data you get back, closely.
+// You will be creating a component for each 'article' in the list.
+// This won't be as easy as just iterating over an array though.
+// Create a function that will programmatically create the following DOM component:
+//
+// <div class="card">
+//   <div class="headline">{Headline of article}</div>
+//   <div class="author">
+//     <div class="img-container">
+//       <img src={url of authors image} />
+//     </div>
+//     <span>By {authors name}</span>
+//   </div>
+// </div>
+//
+// Create a card for each of the articles and add the card to the DOM.
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+  .then(response => {
+    Object.keys(response.data.articles).forEach(topic=>{
+      response.data.articles[topic].forEach(article=>{
+        document.querySelector('.cards-container').appendChild(Card(article));
+      });
+    });
+  })
+  .catch(err => {
+    console.log('Ya messed up cards: ' + err);
+  })
+
+
+function Card(data) {
+  const card = createElement('div');
+  const headline = createElement('div');
+  const author = createElement('div');
+  const photoContainer = createElement('div');
+  const authorPhoto = createElement('img');
+  const authorName = createElement('span');
+
+  card.classList.add('card');
+  headline.classList.add('headline');
+  author.classList.add('author');
+  photoContainer.classList.add('img-container');
+
+  headline.textContent = data.headline;
+  authorName.textContent = "By " + data.authorName;
+
+  authorPhoto.src = data.authorPhoto;
+
+  appendElements(photoContainer, [authorPhoto]);
+  appendElements(author, [photoContainer, authorName]);
+  appendElements(card, [headline, author]);
+
+  return card;
+}
--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,54 +1,76 @@
-class Carousel {
-    constructor(element) {
-        this.element = element;
-        this.leftButton = this.element.querySelector('.left-button');
-        this.rightButton = this.element.querySelector('.right-button');
-        this.images = this.element.querySelectorAll('.carousel img');
-        this.counter = 0;
-        this.amount = this.images.length;
-        this.current = this.images[0];
-        this.current.style.display = 'block';
-    
-        this.rightButton.addEventListener('click', () => {
-            const direction = 1;
-            //this.current.style.opacity = '0';
-            this.current.style.display = 'none';
-           // this.current.style.transform = 'scale(0)';
-           // this.current.style.transition = '1s';
-            this.counter = this.counter + direction;
-            if (!this.images[this.counter]) {
-                this.counter = 0;
-            }
-            this.current = this.images[this.counter];
-            //this.current.style.opacity = '0';
-            this.current.style.display = 'block';
-            //this.current.style.opacity = 1;
-            //this.current.style.transform = 'scale(1)';
-            //this.current.style.transition = '1s';
-
-        });
-    
-        this.leftButton.addEventListener('click', () => {
-            const direction = -1;
-            this.current.style.display = 'none';
-            this.counter = this.counter + direction;
-            if (direction === -1 && this.counter < 0) {
-                this.counter = this.amount - 1;
-            }
-            this.current = this.images[this.counter];
-            this.current.style.display = 'block';
-        });
-    } 
-}
-
-let carousel = document.querySelector('.carousel');
-carousel = new Carousel(carousel);
-
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
-    1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
+    1. You will need to write a function that creates the carousel component, you will find the HTML below.
     2. You will need to grab a reference to all of the images
     3. Create a current index
     4. Those buttons are gonna need some click handlers.
-    5. Think of how you would animate this compoennt. Make the cards slide in and out, or fade. It's up to you!
+    5. Think of how you would animate this component. Make the cards slide in and out, or fade. It's up to you!
     6. Have fun!
 */
+
+/* HTML:
+  <div class="carousel">
+    <div class="left-button"> < </div>
+    <img src="./assets/carousel/mountains.jpeg" />
+    <img src="./assets/carousel/computer.jpeg" />
+    <img src="./assets/carousel/trees.jpeg" />
+    <img src="./assets/carousel/turntable.jpeg" />
+    <div class="right-button"> > </div>
+  </div>
+*/
+const caro = Carousel();
+document.querySelector('.carousel-container').appendChild(caro);
+
+function Carousel() {
+  const carousel = createElement('div');
+  const leftBtn = createElement('div');
+  const img0 = createElement('img');
+  const img1 = createElement('img');
+  const img2 = createElement('img');
+  const img3 = createElement('img');
+  const rightBtn = createElement('div');
+
+  carousel.classList.add('carousel');
+  leftBtn.classList.add('left-button');
+  rightBtn.classList.add('right-button');
+
+  img0.classList.add('show');
+
+  leftBtn.textContent = '<';
+  rightBtn.textContent = '>';
+
+  img0.src = "./assets/carousel/mountains.jpeg"
+  img1.src = "./assets/carousel/computer.jpeg"
+  img2.src = "./assets/carousel/trees.jpeg"
+  img3.src = "./assets/carousel/turntable.jpeg"
+
+  const images = [img0, img1, img2, img3];
+  let i = 0;
+  //console.log(images);
+
+  leftBtn.addEventListener('click', moveLeft);
+  rightBtn.addEventListener('click', moveRight);
+
+  appendElements(carousel, [leftBtn, img0, img1, img2, img3, rightBtn]);
+
+  function moveLeft() {
+    images[i].classList.toggle('show');
+    i--;
+    if (i < 0) {
+      i = 3;
+    }
+    images[i].classList.toggle('show');
+  }
+
+  function moveRight() {
+    images[i].classList.toggle('show');
+    i++;
+    if (i > 3) {
+      i = 0;
+    }
+    images[i].classList.toggle('show');
+  }
+
+  return carousel;
+}
+
+
