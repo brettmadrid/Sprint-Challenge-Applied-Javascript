b5ca6cbe0aea4e9b4d51c4a99a44f72909c0f275
brettmadrid <brettmadrid9@gmail.com>
2019-07-14 16:13:49 -0700

    added carousel and header components

---
+++ components/Header/index.js
@@ -0,0 +1,31 @@
+// STEP 1: Create a header component.
+// -----------------------
+// Using a function create the component you see below:
+//
+//  <div class="header">
+//    <span class="date">SMARCH 28, 2019</span>
+//    <h1>Lambda Times</h1>
+//    <span class="temp">98°</span>
+//  </div >
+// And add it to the DOM in the .headerContainer component
+
+document.querySelector('.header-container').appendChild(Header());
+
+function Header() {
+  const head = createElement('div');
+  const date = createElement('span');
+  const title = createElement('h1');
+  const temp = createElement('span');
+
+  head.classList.add('header');
+  date.classList.add('date');
+  temp.classList.add('temp');
+
+  title.textContent = 'Lambda Times';
+  date.textContent = 'SMARCH 28, 2019';
+  temp.textContent = '98°';
+
+  appendElements(head,[date,title,temp]);
+
+  return head;
+}
